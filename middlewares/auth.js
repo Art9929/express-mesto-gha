5d0af166933df92c074e0ff83a5e6bcc803277a4
Artem <loginryabtsev>
2023-07-16 22:31:11 +0300

    project_14_3

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -9,14 +9,14 @@ function auth(req, res, next) {
   let token = req.headers.cookie;
   let payload = '';
 
-  if (!token) throw new UnauthorizedError('Нет токена');
-  else {
-    try {
-      token = token.split('=');
-      payload = jwt.verify(token[1], JWT_SECRET);
-    } catch {
-      return next(new UnauthorizedError('Ошибка токена (не верный токен)'));
-    }
+  if (!token) {
+    return next(new UnauthorizedError('Нет токена'));
+  }
+  try {
+    token = token.split('=');
+    payload = jwt.verify(token[1], JWT_SECRET);
+  } catch {
+    return next(new UnauthorizedError('Ошибка токена (не верный токен)'));
   }
   req.user = payload;
   return next();
